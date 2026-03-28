28e41ad1c31092ae9e381ae99fdfe10c6ca11a04
sigeyh <rsigeyh28@gmail.com>
2026-03-28 12:18:49 +0300

    \Remove submodule dirs, fix middleware TS"

--- src/app/admin/middleware.tsx
+++ src/app/admin/middleware.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt'; // Optional if using NextAuth, but since Firebase, skip server auth for simplicity
+
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
